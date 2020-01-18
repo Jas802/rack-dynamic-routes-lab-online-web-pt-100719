b4cbe27b29082606e124ac5ee914274a6301fc88
Jas Spencer <jas.spencer414@gmail.com>
2020-01-18 23:59:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,11 +8,14 @@ def call(env)
     item_name = req.path.split("/items/").last
     if item =@@items.find{|i| i.name == item_name}
       resp.write item.price
-    end
     else
       resp.status = 400
-      resp.write "Route not found"
+      resp.write "Item not found"
     end
+  else
+    resp.status = 404
+    resp.write "Route not found"
+  end
     resp.finish
   end
 
